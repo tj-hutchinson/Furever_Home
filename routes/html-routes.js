68499674ca41f03d0010c998aa0767103468a51b
TJ Hutchinson <tjhutch92@gmail.com>
2021-01-19 21:39:45 -0800

    post.js update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-// const path = require("path");
+const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -10,8 +10,8 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("members");
     }
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render("signup");
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.render("signup");
   });
 
   app.get("/login", (req, res) => {
@@ -19,14 +19,14 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("members");
     }
-    // res.sendFile(path.join(__dirname, "../public/login.html"));
-    res.render("login");
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.render("members");
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+    // res.render("members");
   });
 };
--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const passport = require("./config/passport");
 //Password
 require("dotenv").config();
 
-
-var exphbs  = require('express-handlebars');
- 
-
-
+const exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -23,9 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
