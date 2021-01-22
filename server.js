965d375f2bafc66b907a0d59f05196eba51b2c2e
Evan Alioto <EvanJAlioto@gmail.com>
2021-01-21 19:42:07 -0800

    setup handlebars pathing and basic page layout

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const passport = require("./config/passport");
 //Password
 require("dotenv").config();
 
-
 const exphbs = require("express-handlebars");
-
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -17,15 +15,16 @@ const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 
-
-app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs());
 
 // We need to use sessions to keep track of our user's login status
 app.use(
