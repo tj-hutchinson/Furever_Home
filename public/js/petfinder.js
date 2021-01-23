c00d913ac38ea7a214b75e5b1f80985bee702146
TJ Hutchinson <tjhutch92@gmail.com>
2021-01-23 13:06:59 -0800

    Initial Petfinder API Fuctionalility

---
+++ public/js/petfinder.js
@@ -0,0 +1,38 @@
+//get document ready
+$(document).ready(() => {
+  const pf = new petfinder.Client({
+    apiKey: "npUvuNjLj8hvCc6FwTaVD70LxfrPd91k6ovd82KVYrDnWXW0LW",
+    secret: "OzlHCv360zKbVtPXQKnsPhhRZmLBH8WoMPFK55BA"
+  });
+
+  pf.animal.search().then(response => {
+    // Do something with `response.data.animals`
+    response.data.animals
+      .forEach(animal => {
+        console.log(` -- ${animal.name} id: ${animal.id} url: ${animal.url}`);
+      })
+      .catch(error => {
+        // Handle the error
+        console.log(error);
+      });
+  });
+});
+
+//   const petfinder = require("@petfinder/petfinder-js");
+//   const client = new petfinder.Client({
+//     apiKey: "npUvuNjLj8hvCc6FwTaVD70LxfrPd91k6ovd82KVYrDnWXW0LW",
+//     secret: "OzlHCv360zKbVtPXQKnsPhhRZmLBH8WoMPFK55BA"
+//   });
+
+//   client.animal
+//     .search()
+//     .then(resp => {
+//       // Do something with `response.data.animals`
+//       resp.data.animals.forEach(animal => {
+//         console.log(` -- ${animal.name} id: ${animal.id} url: ${animal.url}`);
+//       });
+//     })
+//     .catch(error => {
+//       // Handle the error
+//       console.log(error);
+//     });
